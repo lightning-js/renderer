6b345022ed0213c29f7ac6842bff2cd2a8b205f7
erikhaandrikman <e.haandrikman@metrological.com>
2023-10-25 11:13:42 +0200

    Update dynamic scenario creation

--- backstop.config.cjs
+++ backstop.config.cjs
@@ -1,36 +1,47 @@
 const fs = require('fs');
-const baseConfig = require('./backstop.json');
 
-module.exports = {
-  ...baseConfig,
-  scenarios: getScenariosForProject('./examples/tests/'),
+// exclude following files regression test
+const ignoreList = [
+  'animation.ts',
+  'rotation.ts',
+  'child-positioning.ts',
+  'scale.ts',
+  'test.ts',
+  'text.ts',
+  'textures.ts',
+  'texture-memory-stress.ts',
+];
+
+// grab command line arg port if provided or fallback to default
+// i.e $ npm run backstop 5178
+const port = () => {
+  return process.argv.filter((v) => /\d{4}/.test(v))[0] || '5174';
 };
 
-function getScenariosForProject(projectPath) {
-  const filesToIgnore = [
-    'animation.ts',
-    'rotation.ts',
-    'child-positioning.ts',
-    'scale.ts',
-    'test.ts',
-    'text.ts',
-    'textures.ts',
-    'texture-memory-stress.ts',
-  ];
-  const files = fs
-    .readdirSync(projectPath)
-    .filter((file) => !filesToIgnore.includes(file));
-  console.log(files);
-  return files.map((file) => {
-    const scenarioLabel = file.split('.')[0].split('-').join(' ');
-    const testName = file.split('.')[0];
+const resolveScenarios = (path, ignore) => {
+  return fs
+    .readdirSync(path)
+    .filter((file) => {
+      return !ignore.includes(`${file}`);
+    })
+    .map((file) => {
+      const getFile = /^([-\w]+)\./;
+      const [match, fileName] = getFile.exec(file);
 
-    return {
-      label: scenarioLabel,
-      url: `http://localhost:5174/?test=${testName}`,
-      readyEvent: 'ready!',
-      delay: 100,
-      misMatchThreshold: 0.1,
-    };
-  });
-}
+      if (!fileName) {
+        return;
+      }
+
+      return {
+        label: fileName.replace('-', ' '),
+        url: `http://localhost:${port()}/?test=${fileName}`,
+        readyEvent: 'ready!',
+        misMatchThreshold: 0.1,
+      };
+    });
+};
+
+module.exports = {
+  ...require('./backstop.json'),
+  scenarios: resolveScenarios('./examples/tests/', ignoreList),
+};
