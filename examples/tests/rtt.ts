ca6d7cef3a1a7b808d3ae9806f53d0442d3f33dc
erikhaandrikman <e.haandrikman@metrological.com>
2024-02-21 15:51:28 +0100

    Update RTT examples

--- examples/tests/rtt.ts
+++ examples/tests/rtt.ts
@@ -1,4 +1,25 @@
 import type { ExampleSettings } from '../common/ExampleSettings.js';
+interface AnimationExampleSettings {
+  duration: number;
+  easing: string;
+  delay: number;
+  loop: boolean;
+  stopMethod: 'reverse' | 'reset' | false;
+}
+
+const animationSettings: Partial<AnimationExampleSettings> = {
+  duration: 3000,
+  delay: 400,
+  loop: false,
+  stopMethod: 'reverse',
+  easing: 'ease-in-out-back',
+};
+
+const randomColor = () => {
+  const randomInt = Math.floor(Math.random() * Math.pow(2, 32));
+  const hexString = randomInt.toString(16).padStart(8, '0');
+  return parseInt(hexString, 16);
+};
 
 export default async function ({ renderer, testRoot }: ExampleSettings) {
   const node = renderer.createNode({
@@ -19,40 +40,65 @@ export default async function ({ renderer, testRoot }: ExampleSettings) {
     text: 'Render to Texture',
   });
 
-  const rttNode = renderer.createNode({
-    x: 30,
-    y: 110,
-    width: 1500,
-    height: 1500,
-    parent: node,
-    color: 0x0000ffff,
+  const rootRenderToTextureNode = renderer.createNode({
+    x: 0,
+    y: 0,
+    width: 1920,
+    height: 1080,
+    parent: testRoot,
     rtt: true,
+    zIndex: 5,
+    colorTop: randomColor(),
+    colorBottom: randomColor(),
   });
 
-  const rttNodeImage = renderer.createNode({
-    x: 30,
-    y: 110,
-    width: 500,
-    height: 500,
-    parent: rttNode,
-    src: '../assets/rocko.png',
+  const rootChildRectangle = renderer.createNode({
+    x: 400,
+    y: 40,
+    width: 50,
+    height: 50,
+    color: 0x0ffffffff,
+    parent: testRoot,
   });
 
-  // const rttChild1 = renderer.createTextNode({
-  //   parent: rttNode,
-  //   x: 120,
-  //   y: 510,
-  //   fontFamily: 'Ubuntu',
-  //   fontSize: 20,
-  //   text: 'RTT child text',
-  // });
+  new Array(90).fill(0).forEach((_, i) => {
+    renderer.createNode({
+      parent: rootRenderToTextureNode,
+      x: (i % 15) * 120 + 50,
+      y: Math.floor(i / 15) * 140 + 150,
+      width: 120,
+      height: 120,
+      src: '../assets/rocko.png',
+    });
+  });
 
-  // const rttChild2 = renderer.createNode({
-  //   parent: rttNode,
-  //   x: 265,
-  //   y: 512,
-  //   color: 0xffffffff,
-  //   width: 20,
-  //   height: 20,
-  // });
+  const rttRotate = rootRenderToTextureNode.animate(
+    {
+      rotation: Math.PI / 8,
+      scale: 2.2,
+    },
+    animationSettings,
+  );
+
+  rttRotate.start();
+
+  const rootRectRotate = rootChildRectangle.animate(
+    {
+      rotation: Math.PI / 4,
+      scale: 1.2,
+    },
+    { ...animationSettings, loop: true },
+  );
+  rootRectRotate.start();
+
+  setInterval(() => {
+    const colorize = rootRenderToTextureNode.animate(
+      {
+        colorTop: randomColor(),
+        colorBottom: randomColor(),
+      },
+      { easing: 'ease-in', duration: 1500 },
+    );
+    colorize.start();
+  }, 5000);
 }
