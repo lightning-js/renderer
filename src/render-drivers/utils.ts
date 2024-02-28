748075249caa9099ce8a73a31a3295fa7be85af3
wouterlucas <wouter@wouterlucas.com>
2024-02-28 13:51:32 +0100

    Custom data: minor perf updates to santizeCustomDataMap

--- src/render-drivers/utils.ts
+++ src/render-drivers/utils.ts
@@ -58,21 +58,34 @@ export async function loadCoreExtension(
 }
 
 export function santizeCustomDataMap(d: CustomDataMap): CustomDataMap {
-  for (const key in d) {
-    const value = d[key];
+  const validTypes = { boolean: true, string: true, number: true };
 
-    if (typeof value === 'string' && value.length > 2048) {
+  const keys = Object.keys(d);
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    if (!key) {
+      continue;
+    }
+
+    const value = d[key];
+    const valueType = typeof value;
+
+    // Typescript doesn't understand the above const valueType ¯\_(ツ)_/¯
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore-next-line
+    if (valueType === 'string' && value.length > 2048) {
       console.warn(
         `Custom Data value for ${key} is too long, it will be truncated to 2048 characters`,
       );
+
+      // same here, see above comment, this can only be a string at this point
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore-next-line
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
       d[key] = value.substring(0, 2048);
     }
 
-    if (
-      typeof value !== 'boolean' &&
-      typeof value !== 'string' &&
-      typeof value !== 'number'
-    ) {
+    if (!validTypes[valueType as keyof typeof validTypes]) {
       console.warn(
         `Custom Data value for ${key} is not a boolean, string, or number, it will be ignored`,
       );
