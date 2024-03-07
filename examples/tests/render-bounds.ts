a37767d8b1b465e7bb0a8b1d0d2a23dd730313da
Wouter lucas van Boesschoten <wouter@wouterlucas.com>
2024-03-07 21:58:47 +0100

    Render bounds: Fix test.

--- examples/tests/render-bounds.ts
+++ examples/tests/render-bounds.ts
@@ -7,15 +7,15 @@ export default async function ({ renderer, testRoot }: ExampleSettings) {
     return (Math.PI / 180) * deg;
   };
 
-  // const greenRect = renderer.createNode({
-  //   x: 0,
-  //   y: 540,
-  //   width: 960,
-  //   height: 540,
-  //   color: 0x00ff00ff,
-  //   clipping: true,
-  //   parent: testRoot,
-  // });
+  const greenRect = renderer.createNode({
+    x: 0,
+    y: 540,
+    width: 960,
+    height: 540,
+    color: 0x00ff00ff,
+    clipping: true,
+    parent: testRoot,
+  });
 
   const redRect = renderer.createNode({
     // skipRender: true,
@@ -29,12 +29,12 @@ export default async function ({ renderer, testRoot }: ExampleSettings) {
     parent: testRoot,
   });
 
-  redRect.onOutOfBounds(() => {
-    console.log('red rect outside render bounds');
+  redRect.onOutOfViewport(() => {
+    console.log('red rect outside view port');
   });
 
-  redRect.onInBounds(() => {
-    console.log('red rect in render bounds');
+  redRect.onInViewport(() => {
+    console.log('red rect in view port');
   });
 
   const text = renderer.createTextNode({
@@ -42,89 +42,89 @@ export default async function ({ renderer, testRoot }: ExampleSettings) {
     parent: testRoot,
   });
 
-  // let redRectAnimation: IAnimationController | null = null;
-  // // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  // setTimeout(async () => {
-  //   // eslint-disable-next-line no-constant-condition
-  //   while (true) {
-  //     redRectAnimation = redRect
-  //       .animate(
-  //         {
-  //           rotation: degToRad(0),
-  //         },
-  //         {
-  //           delay: 2000,
-  //           duration: 2000,
-  //         },
-  //       )
-  //       .start();
-  //     await redRectAnimation.waitUntilStopped();
+  let redRectAnimation: IAnimationController | null = null;
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  setTimeout(async () => {
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      redRectAnimation = redRect
+        .animate(
+          {
+            x: 0,
+          },
+          {
+            delay: 2000,
+            duration: 2000,
+          },
+        )
+        .start();
+      await redRectAnimation.waitUntilStopped();
 
-  //     redRectAnimation = redRect
-  //       .animate(
-  //         {
-  //           rotation: degToRad(90),
-  //         },
-  //         {
-  //           duration: 2000,
-  //         },
-  //       )
-  //       .start();
-  //     await redRectAnimation.waitUntilStopped();
-  //   }
-  // }, 1000);
+      redRectAnimation = redRect
+        .animate(
+          {
+            x: -205,
+          },
+          {
+            duration: 2000,
+          },
+        )
+        .start();
+      await redRectAnimation.waitUntilStopped();
+    }
+  }, 1000);
 
-  // const blueRect = renderer.createNode({
-  //   x: 1921,
-  //   y: 540,
-  //   width: 540,
-  //   height: 540,
-  //   color: 0x0000ffff,
-  //   mountX: 0.5,
-  //   mountY: 0.5,
-  //   rotation: degToRad(45 + 180),
-  //   parent: greenRect,
-  // });
+  const blueRect = renderer.createNode({
+    x: 1921,
+    y: 540,
+    width: 540,
+    height: 540,
+    color: 0x0000ffff,
+    mountX: 0.5,
+    mountY: 0.5,
+    rotation: degToRad(45 + 180),
+    parent: greenRect,
+  });
 
-  // blueRect.onOutOfRenderBounds(() => {
-  //   console.log('blue rect outside render bounds');
-  // });
+  blueRect.onOutOfBounds(() => {
+    console.log('blue rect outside render bounds');
+  });
 
-  // blueRect.onInRenderBounds(() => {
-  //   console.log('blue rect in render bounds');
-  // });
+  blueRect.onInBounds(() => {
+    console.log('blue rect in render bounds');
+  });
 
-  // let blueRectAnimation: IAnimationController | null = null;
-  // // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  // setTimeout(async () => {
-  //   // eslint-disable-next-line no-constant-condition
-  //   while (true) {
-  //     blueRectAnimation = blueRect
-  //       .animate(
-  //         {
-  //           rotation: degToRad(360),
-  //           x: 1000,
-  //         },
-  //         {
-  //           delay: 1000,
-  //           duration: 2000,
-  //         },
-  //       )
-  //       .start();
-  //     await blueRectAnimation.waitUntilStopped();
+  let blueRectAnimation: IAnimationController | null = null;
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  setTimeout(async () => {
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      blueRectAnimation = blueRect
+        .animate(
+          {
+            rotation: degToRad(360),
+            x: 1000,
+          },
+          {
+            delay: 1000,
+            duration: 2000,
+          },
+        )
+        .start();
+      await blueRectAnimation.waitUntilStopped();
 
-  //     blueRectAnimation = blueRect
-  //       .animate(
-  //         {
-  //           rotation: degToRad(45),
-  //           x: 2400,
-  //         },
-  //         {
-  //           duration: 2000,
-  //         },
-  //       )
-  //       .start();
-  //     await blueRectAnimation.waitUntilStopped();
-  //   }
-  // }, 1000);
+      blueRectAnimation = blueRect
+        .animate(
+          {
+            rotation: degToRad(45),
+            x: 2400,
+          },
+          {
+            duration: 2000,
+          },
+        )
+        .start();
+      await blueRectAnimation.waitUntilStopped();
+    }
+  }, 1000);
 }
