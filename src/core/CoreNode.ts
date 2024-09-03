3a86fe873ae712523196d27f11c60cf244be71a5
erikhaandrikman <e.haandrikman@metrological.com>
2024-09-03 20:26:50 +0200

    Reordered CoreNode renderTexture test

--- src/core/CoreNode.ts
+++ src/core/CoreNode.ts
@@ -959,6 +959,26 @@ export class CoreNode extends EventEmitter {
     const parent = this.props.parent;
     let childUpdateType = UpdateType.None;
 
+    if (this.updateType & UpdateType.ParentRenderTexture) {
+      let p = this.parent;
+      while (p) {
+        if (p.rtt) {
+          this.parentHasRenderTexture = true;
+        }
+        p = p.parent;
+      }
+    }
+
+    // If we have render texture updates and not already running a full update
+    if (
+      this.updateType ^ UpdateType.All &&
+      this.updateType & UpdateType.RenderTexture
+    ) {
+      this.children.forEach((child) => {
+        child.setUpdateType(UpdateType.All);
+      });
+    }
+
     if (this.updateType & UpdateType.Global) {
       assertTruthy(this.localTransform);
 
@@ -1005,26 +1025,6 @@ export class CoreNode extends EventEmitter {
     if (this.renderState === CoreNodeRenderState.OutOfBounds) {
       this.updateType = 0;
       return;
-    }
-
-    if (this.updateType & UpdateType.ParentRenderTexture) {
-      let p = this.parent;
-      while (p) {
-        if (p.rtt) {
-          this.parentHasRenderTexture = true;
-        }
-        p = p.parent;
-      }
-    }
-
-    // If we have render texture updates and not already running a full update
-    if (
-      this.updateType ^ UpdateType.All &&
-      this.updateType & UpdateType.RenderTexture
-    ) {
-      this.children.forEach((child) => {
-        child.setUpdateType(UpdateType.All);
-      });
     }
 
     if (this.updateType & UpdateType.Clipping) {
