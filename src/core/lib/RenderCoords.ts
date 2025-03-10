8c81ba77aca6978ad02a16109bac465dc3724916
jfboeve <jeffrey_boeve@comcast.com>
2025-03-10 14:16:32 +0100

    remove commented code + added license block

--- src/core/lib/RenderCoords.ts
+++ src/core/lib/RenderCoords.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Comcast Cable Communications Management, LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 export class RenderCoords {
   public x1: number;
   public y1: number;
@@ -53,90 +69,3 @@ export class RenderCoords {
     return out;
   }
 }
-
-// const rx1 = 0;
-// const rx2 = 2;
-// const rx3 = 4;
-// const rx4 = 6;
-// const ry1 = 1;
-// const ry2 = 3;
-// const ry3 = 5;
-// const ry4 = 7;
-
-// export class RenderCoords {
-//   public data: Float32Array;
-//   constructor(
-//     entries?:
-//       | [number, number, number, number, number, number, number, number]
-//       | Float32Array,
-//   ) {
-//     this.data = new Float32Array(8);
-//     if (entries) {
-//       this.data[rx1] = entries[rx1];
-//       this.data[rx2] = entries[rx2];
-//       this.data[rx3] = entries[rx3];
-//       this.data[rx4] = entries[rx4];
-//       this.data[ry1] = entries[ry1];
-//       this.data[ry2] = entries[ry2];
-//       this.data[ry3] = entries[ry3];
-//       this.data[ry4] = entries[ry4];
-//     }
-//   }
-
-//   public static translate(
-//     x1: number,
-//     y1: number,
-//     x2: number,
-//     y2: number,
-//     x3: number,
-//     y3: number,
-//     x4: number,
-//     y4: number,
-//     out?: RenderCoords,
-//   ): RenderCoords {
-//     if (!out) {
-//       out = new RenderCoords();
-//     }
-//     out.data[rx1] = x1;
-//     out.data[rx2] = x2;
-//     out.data[rx3] = x3;
-//     out.data[rx4] = x4;
-//     out.data[ry1] = y1;
-//     out.data[ry2] = y2;
-//     out.data[ry3] = y3;
-//     out.data[ry4] = y4;
-//     return out;
-//   }
-
-//   get x1(): number {
-//     return this.data[rx1]!;
-//   }
-
-//   get x2(): number {
-//     return this.data[rx2]!;
-//   }
-
-//   get x3(): number {
-//     return this.data[rx3]!;
-//   }
-
-//   get x4(): number {
-//     return this.data[rx4]!;
-//   }
-
-//   get y1(): number {
-//     return this.data[ry1]!;
-//   }
-
-//   get y2(): number {
-//     return this.data[ry2]!;
-//   }
-
-//   get y3(): number {
-//     return this.data[ry3]!;
-//   }
-
-//   get y4(): number {
-//     return this.data[ry4]!;
-//   }
-// }
