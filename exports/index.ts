ae74cc7addb2a67c142ac34c1235240f5ec1cdd2
Mirko Pecora <mi.pecora@reply.it>
2025-10-10 16:11:45 +0200

    fix: export TextureError

--- exports/index.ts
+++ exports/index.ts
@@ -51,9 +51,10 @@ export {
   type TextureMap,
 } from '../src/core/CoreTextureManager.js';
 export type { MemoryInfo } from '../src/core/TextureMemoryManager.js';
-export type {
+export {
   TextureError,
   TextureErrorCode,
+  isTextureError,
 } from '../src/core/TextureError.js';
 export type { ShaderMap, EffectMap } from '../src/core/CoreShaderManager.js';
 export type { TextRendererMap } from '../src/core/text-rendering/renderers/TextRenderer.js';
