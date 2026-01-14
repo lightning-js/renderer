677155104fb6e8a974da78c3f31787d6904020b7
Mirko Pecora <33352553+pecoram@users.noreply.github.com>
2026-01-14 10:37:19 +0100

    Fix text width handling in inspector (#672)
    - Adjust the width and height properties to return 'auto' instead of
    '1px' for zero values.
    - Update the inspector's text handling to use visibility instead of
    color for inspector text visible during color transitions.
    
    ---------
    
    Co-authored-by: jfboeve <Jeffrey_Boeve@comcast.com>

--- src/main-api/Inspector.ts
+++ src/main-api/Inspector.ts
@@ -52,16 +52,14 @@ const stylePropertyMap: {
   },
   width: (w) => {
     if (w === 0) {
-      // Set to 1px instead of 0px so visibility checks (e.g. Playwright) keep treating the node as visible
-      return { prop: 'width', value: '1px' };
+      return { prop: 'width', value: 'auto' };
     }
 
     return { prop: 'width', value: `${w}px` };
   },
   height: (h) => {
     if (h === 0) {
-      // Set to 1px instead of 0px so visibility checks (e.g. Playwright) keep treating the node as visible
-      return { prop: 'height', value: '1px' };
+      return { prop: 'height', value: 'auto' };
     }
 
     return { prop: 'height', value: `${h}px` };
