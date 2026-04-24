f156a9609a1a571d6aeb0d345c082d9c5ae9c9b3
jfboeve <jeffrey_boeve@comcast.com>
2026-04-24 10:16:21 +0200

    update test comments

--- examples/tests/text-empty-string.ts
+++ examples/tests/text-empty-string.ts
@@ -8,25 +8,27 @@ export default async function ({ renderer, testRoot }: ExampleSettings) {
     parent: testRoot,
   });
 
-  // doesn't clear the text node
+  //set the text with empty string
   setTimeout(() => {
-    //check with multiple newlines no words
     text.text = '';
   }, 1000);
 
+  //set the text again after a short delay
   setTimeout(() => {
     text.text = 'Hello again';
   }, 2000);
 
+  //set text with a single space character
   setTimeout(() => {
-    //check with a single space character
     text.text = ' ';
   }, 3000);
 
+  //set the text again after a short delay
   setTimeout(() => {
     text.text = 'Hello again two';
   }, 4000);
 
+  //set text with a zero-width space character
   setTimeout(() => {
     text.text = '\u200B'; // zero-width space
   }, 5000);
