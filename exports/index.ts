13a26afd86ecf5830b0a44e8d81750268f35bfbf
Guilherme Simoes <guilherme.rdems@gmail.com>
2026-06-24 13:07:16 +0100

    Format

--- exports/index.ts
+++ exports/index.ts
@@ -59,8 +59,15 @@ export type { MemoryInfo } from '../src/core/TextureMemoryManager.js';
 export type { AnimationSettings } from '../src/core/animations/CoreAnimation.js';
 export type { TimingFunction } from '../src/core/utils.js';
 export type { Inspector } from '../src/main-api/Inspector.js';
-export { CoreNode, type CoreNodeProps, type CoreNodeRenderState } from '../src/core/CoreNode.js';
-export { CoreTextNode, type CoreTextNodeProps } from '../src/core/CoreTextNode.js';
+export {
+  CoreNode,
+  type CoreNodeProps,
+  type CoreNodeRenderState,
+} from '../src/core/CoreNode.js';
+export {
+  CoreTextNode,
+  type CoreTextNodeProps,
+} from '../src/core/CoreTextNode.js';
 
 export * from '../src/core/renderers/CoreShaderNode.js';
 export * from '../src/core/shaders/templates/BorderTemplate.js';
